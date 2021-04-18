2c4877f079d1283e89f9ad121cfcfa7a06daca00
Pierre Paillard <paillard.pierre@gmail.com>
2021-04-18 16:16:55 +0200

    fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HelloWorldService } from './hello-world.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularServicesRetourExercice';
+  private service: HelloWorldService;
+
+  constructor(param_service: HelloWorldService) {
+    this.service = param_service;
+  }
+  ngOnInit(): void {
+    this.service.getHelloWorld().subscribe((param_msg: string) => {
+      this.title = param_msg;
+    });
+  }
 }
